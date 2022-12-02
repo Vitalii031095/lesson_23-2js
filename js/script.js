32aeba57f36f7fba9b678fee35d07b3ca4c54e46
Vitalii <112901030+Vitalii031095@users.noreply.github.com>
2022-12-02 11:25:17 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+
+console.log(Math.floor(58.858))
+
+// let text = 'фрилансер';
+// console.log(text[5]);
+
+// let text = 'фрилансер';
+// // console.log(text.toUpperCase())
+
+// let text = 'фрилансер';
+// console.log(text.slice(3,6))
+let text = 'фрилансер';
+console.log(text.includes('лан', 3)); 
